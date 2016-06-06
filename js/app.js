8c25a572b62c8bdcd7ad4dd4d328f256196c5b47
Bruno Opsenica <bruno@functionalimperative.com>
2016-06-06 16:20:28 -0400

    Refactor and clean up code

--- js/app.js
+++ js/app.js
@@ -1,59 +1,35 @@
 var container, stats;
 
 var camera, scene, renderer, mesh;
-var orientations;
-
 
 function init() {
   var height = window.innerHeight;
   var width  = window.innerWidth;
-  var ROW_COUNT = 20;
-  var boxLengthInPixels = height / ROW_COUNT;
-  var COLUMN_COUNT = Math.ceil(width / boxLengthInPixels);
-
+  
   container = document.getElementById( 'container' );
   
-  camera = new THREE.OrthographicCamera( -.5 * width, .5 * width, .5 * height, -.5 * height, 1, boxLengthInPixels * (COLUMN_COUNT + ROW_COUNT + 1) );
-  camera.position.z = boxLengthInPixels * 2;
+  var boxGrid = new BoxGrid(width, height);
+  
+  camera = new THREE.OrthographicCamera( 
+    -.5 * width,
+    .5 * width,
+    .5 * height,
+    -.5 * height,
+    1,
+    boxGrid.boxLengthInPixels * (boxGrid.columnCount + boxGrid.rowCount + 2) );
+  
+  camera.position.z = boxGrid.boxLengthInPixels * 2;
 
   renderer = new THREE.WebGLRenderer();
   scene = new THREE.Scene();
 
-  // geometry
-
-  var instances = ROW_COUNT * COLUMN_COUNT;
-
-  var boxGeometry = new THREE.BoxGeometry(
-    boxLengthInPixels, boxLengthInPixels, boxLengthInPixels, //Side lengths
-    1, 1, 1 //Number of tri's per face (N + 1)
-  );
-  var geometry = new THREE.InstancedBufferGeometry().fromGeometry(boxGeometry);
-
-  // per instance data
-  var offsets = new THREE.InstancedBufferAttribute( new Float32Array( instances * 3 ), 3, 1 );
-  var dx = (boxLengthInPixels / width);
-  var dy = 1 / ROW_COUNT;
-  for ( var i = 0, ul = offsets.count; i < ul; i++ ) {
-    var columnIndex = (i % COLUMN_COUNT)
-    var rowIndex    = Math.floor(i / COLUMN_COUNT)
-    var distanceFromCenterX = columnIndex - (.5 * COLUMN_COUNT);
-    var distanceFromCenterY = rowIndex - (.5 * ROW_COUNT);
-    
-    var x = width  * (dx * (distanceFromCenterX + 0.5));
-    var y = height * (dy * (distanceFromCenterY + 0.5));
-    var z = (Math.abs(Math.ceil(distanceFromCenterX)) 
-            + Math.abs(Math.ceil(distanceFromCenterY)))
-            * -boxLengthInPixels;
-
-    offsets.setXYZ( i, x, y, z );
-  }
-
-  geometry.addAttribute( 'offset', offsets ); // per mesh translation
+  var geometry = boxGrid.geometry;
 
   // material
   var rotationField = new THREE.TextureLoader().load( 'textures/rotationField.png' );
-  var texture = new THREE.TextureLoader().load( 'textures/crate.gif' );
-  texture.anisotropy = renderer.getMaxAnisotropy();
+  var texture = new THREE.TextureLoader().load( 'textures/cubeMap.png' );
+  texture.magFilter = THREE.NearestFilter;
+	texture.minFilter = THREE.NearestFilter;
 
   var material = new THREE.RawShaderMaterial( {
     uniforms: {
@@ -65,19 +41,11 @@ function init() {
     },
     vertexShader: document.getElementById( 'vertexShader' ).textContent,
     fragmentShader: document.getElementById( 'fragmentShader' ).textContent,
-    side: THREE.DoubleSide,
     transparent: false
-
   } );
 
   mesh = new THREE.Mesh( geometry, material );
   scene.add( mesh );
-
-
-  if ( renderer.extensions.get( 'ANGLE_instanced_arrays' ) === false ) {
-    document.getElementById( "notSupported" ).style.display = "";
-    return;
-  }
 
   renderer.setClearColor( 0x101010 );
   renderer.setPixelRatio( window.devicePixelRatio );
@@ -88,7 +56,11 @@ function init() {
   container.appendChild( stats.dom );
 
   window.addEventListener( 'resize', onWindowResize, false );
-
+  
+  if ( renderer.extensions.get( 'ANGLE_instanced_arrays' ) === false ) {
+    alert( "You are missing support for 'ANGLE_instanced_arrays'" );
+    return;
+  }
 }
 
 function onWindowResize( event ) {
@@ -112,15 +84,10 @@ function animate() {
 
 }
 
-var lastTime = 0;
-
-var moveQ = ( new THREE.Quaternion( .5, .5, .5, 0.0 ) ).normalize();
-var tmpQ = new THREE.Quaternion();
-var currentQ = new THREE.Quaternion();
 function render() {
 
   var object = scene.children[0];
-  mesh.material.uniforms.time.value += 0.005
+  mesh.material.uniforms.time.value += 0.005;
   renderer.render( scene, camera );
 }
 
