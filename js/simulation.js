1f31148e946afeb69e894afb2aa08625b5a62354
Bruno Opsenica <bruno@functionalimperative.com>
2016-06-08 10:12:40 -0400

    Add in simulation js files, integration pending

---
+++ js/simulation.js
@@ -0,0 +1,100 @@
+"use strict";
+
+function Simulation(width, height, shaderHash) {
+    //Setting constants
+    this.width = width;
+    this.height = height;
+    this.shaderHash = shaderHash;
+
+    this.setSimUniform = function(uniformName, newValue) {
+      return SimulationUtilities.setUniform(this.simulationMesh, uniformName, newValue)
+    };
+
+    this.passThroughRender = function(input, output) {
+      SimulationUtilities.setUniform(this.passThroughMesh, 'texture', input);
+      if (!output) {
+        this.renderer.render(this.passThroughScene, this.orthoCamera);
+      } else {
+        this.renderer.render(this.passThroughScene, this.orthoCamera, output);
+      }
+    };
+
+    this.update = function() {
+      //render the particles at the new location
+      this.renderer.render( this.simulationScene, this.orthoCamera, this.rtPositionNew);
+      this.setSimUniform("position_texture", this.rtPositionNew.texture);
+      this.renderer.render( this.simulationScene, this.orthoCamera, this.rtPositionCur);
+      this.setSimUniform("position_texture", this.rtPositionCur.texture);
+      
+      this.setSimUniform('mouse_magnitude', 0.0);
+    }
+    
+    this.onMouseClick = function(event) {
+      var mouse = new THREE.Vector2(event.offsetX / this.width, 1 - (event.offsetY / this.height));
+      // raycaster.setFromCamera(mouse, orthoCamera);
+      // 
+      // var intersects = raycaster.intersectObjects(scene.children);
+      // if (intersects.length > 0) {
+      //   intersect.copy(intersects[0].uv);
+      // }
+      this.setSimUniform('mouse', mouse);
+      this.setSimUniform('mouse_magnitude', 1);
+    };
+    
+    this.init = function() {
+      this.raycaster = new THREE.Raycaster();
+      this.intersect = new THREE.Vector2(0.5, 0.5);
+      this.mouse = new THREE.Vector2();
+      
+      this.renderer = new THREE.WebGLRenderer();
+      this.renderer.setSize(this.width, this.height);
+      
+      var gl = this.renderer.getContext();
+
+      //1 we need FLOAT Textures to store positions
+      if (!gl.getExtension("OES_texture_float")) {
+          throw new Error( "float textures not supported" );
+      }
+      
+      this.simulationScene  = new THREE.Scene();
+      this.passThroughScene = new THREE.Scene();
+      this.orthoCamera = new THREE.OrthographicCamera(-1,1,1,-1,1/Math.pow( 2, 53 ),1 );
+
+      var passThroughMaterial = new THREE.ShaderMaterial({
+        uniforms: {
+      		texture: { type: "t", value: null }
+        },
+        vertexShader: this.shaderHash.passThrough.vertex,
+        fragmentShader: this.shaderHash.passThrough.fragment,
+      });
+
+      //5 the simulation:
+      //create a bi-unit quadrilateral and uses the simulation material to update the Float Texture
+      var geom = SimulationUtilities.getBiUnitPlane();
+      this.passThroughMesh = new THREE.Mesh(geom, passThroughMaterial);
+      this.passThroughScene.add(this.passThroughMesh);
+
+      [this.rtPositionCur, this.rtPositionNew] = SimulationUtilities.getRenderTargets(this, this.width, this.height);
+      
+      var simulationMaterial = new THREE.ShaderMaterial({
+        uniforms: {
+          position_texture: { type: 't', value: this.rtPositionCur },
+          mouse: { type: "v2", value: this.intersect },
+          dx: { type: 'f', value: 1/this.width },
+          dy: { type: 'f', value: 1/this.height },
+          width: { type: 'f', value: this.width },
+          height: { type: 'f', value: this.height },
+          wave_speed: { type: 'f', value: 0.7 / Math.min(this.width, this.height) },
+          damping_strength: { type: 'f', value: 0.005 },
+          mouse_magnitude: { type: "f", value: 0.0 },
+          draw_radius: { type: "f", value: 10 / this.width }
+        },
+        vertexShader: this.shaderHash.simulation.vertex,
+        fragmentShader: this.shaderHash.simulation.fragment
+      });
+      
+      geom = SimulationUtilities.getBiUnitPlane();
+      this.simulationMesh = new THREE.Mesh(geom, simulationMaterial);
+      this.simulationScene.add(this.simulationMesh);
+    }
+};
---
+++ shaders/simulation/fragment.glsl
@@ -0,0 +1,73 @@
+precision highp float;
+
+varying vec2 vUv;
+
+uniform float wave_speed;
+uniform float damping_strength;
+uniform float mouse_magnitude;
+uniform float draw_radius;
+uniform float dx;
+uniform float dy;
+uniform float width;
+uniform float height;
+uniform vec2 mouse;
+
+uniform sampler2D position_texture;
+
+vec4 get_texture_values(in vec2 tex_coords) {
+  return texture2D(position_texture, tex_coords);
+}
+
+float fd_central(in float left, in float center, in float right, in float offset) {
+  return (right + left - (2.0 * center)) * pow(offset, -2.);
+}
+
+float get_x_deriv(in float center) {
+  vec2 left_uv  = vUv - vec2(dx, 0.);
+  vec2 right_uv = vUv + vec2(dx, 0.);
+  float sign_left  = sign(left_uv.x);
+  float sign_right = sign(1. - right_uv.x);
+  float left   = clamp(-sign_left, 0., 1.) + sign_left * get_texture_values(left_uv).r;
+  float right  = clamp(-sign_right, 0., 1.) + sign_right * get_texture_values(right_uv).r;
+  return fd_central(left, center, right, dx);
+}
+
+float get_y_deriv(in float center) {
+  vec2 top_uv    = vUv + vec2(0., dy);
+  vec2 bottom_uv = vUv - vec2(0., dy);
+  float sign_top = sign(1. - top_uv.y);
+  float sign_bot = sign(bottom_uv.y);
+  float top    = clamp(-sign_top, 0., 1.) + sign_top * get_texture_values(top_uv).r;
+  float bottom = clamp(-sign_bot, 0., 1.) + sign_bot * get_texture_values(bottom_uv).r;
+  return fd_central(bottom, center, top, dy);
+}
+
+float get_next_position() {
+  float cur_position = get_texture_values(vUv).r;
+  float old_position = get_texture_values(vUv).g;
+  vec2 gradient = vec2(get_x_deriv(cur_position), get_y_deriv(cur_position));
+  
+  vec2 wave_speeds = pow(wave_speed, 2.) * vec2(pow(height/width, 2.), 1.);
+  
+  float damping_factor = 1. / (1. + damping_strength);
+  return damping_factor * (
+      (damping_strength - 1.) * old_position
+      + 2. * cur_position
+      + dot(wave_speeds, gradient));
+}
+
+float calculate_mouse_impact(vec2 uv) {
+  // We need to scale our distance depending on our height/width
+  float mouse_distance = length(vec2(1, height/width) * vec2(mouse - uv));
+  return mouse_magnitude * 0.5 * max(sign(draw_radius - mouse_distance), 0.0);
+  
+}
+
+
+void main() {
+  float cur_position = get_texture_values(vUv).r;
+  float new_position = get_next_position();
+  float mouse_impact = calculate_mouse_impact(vUv);
+  float final_value = clamp(new_position + mouse_impact, 0., 1.);
+  gl_FragColor = vec4(final_value, cur_position, 0.0, 1.0);
+}
